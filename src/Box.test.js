a255ac16586343dbb82123aa873e1335f1661f69
Philip Johnson <p.zhao.johnson@gmail.com>
2024-04-05 21:58:46 -0400

    Add matching static method

--- src/Box.test.js
+++ src/Box.test.js
@@ -1,6 +1,17 @@
 import { Box } from './Box';
 
 describe('Box class', () => {
+  test('matching static method', () => {
+    let box1 = { x: 57.9, y: 223.44, width: -507.221, height: 1044.922 };
+    let box2 = Box.matching(box1);
+
+    let { x, y, width, height } = box2;
+    expect({ x, y, width, height }).toStrictEqual(box1);
+
+    // created a new object
+    expect(box2).not.toBe(box1);
+  });
+
   test('constructor', () => {
     let box = new Box(15.2, -28.3, -101.559, 102.823);
 
--- src/Box.ts
+++ src/Box.ts
@@ -1,7 +1,27 @@
 /**
+ * Something like a box.
+ */
+type BoxLike = {
+  readonly x: number;
+  readonly y: number;
+  readonly width: number;
+  readonly height: number;
+};
+
+/**
  * Represents a box (such as things returned by methods like `getBBox` and `getBoundingClientRect`).
  */
 export class Box {
+  /**
+   * Creates and returns a new box with the same X and Y origin coordinates and width and height
+   * as the input box-like object.
+   */
+  static matching(box: BoxLike): Box {
+    let { x, y, width, height } = box;
+
+    return new Box(x, y, width, height);
+  }
+
   /**
    * The width and height of the box are permitted to be negative.
    *
